653adf77f0e48fecfd0e8f9be39d454bf58cca5d
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-04-28 10:14:39 -0400

    Created a few classes for keeping track of

---
+++ Accounting_Pro_JA_CAP_1/src/main/java/com/pluralsight/Credit_and_Debit_Checker.java
@@ -0,0 +1,4 @@
+package com.pluralsight;
+
+public class Credit_and_Debit_Checker {
+}
--- Accounting_Pro_JA_CAP_1/src/main/java/com/pluralsight/Main.java
+++ Accounting_Pro_JA_CAP_1/src/main/java/com/pluralsight/Main.java
@@ -7,21 +7,25 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Reader_and_Writer readerAndWriter = new Reader_and_Writer("transactions.csv");
 
-        //Deposit
-        System.out.println("What is your Name: ");
-        String name = scanner.nextLine();
-
-        System.out.println("Card Type Debit or Credit: ");
-        String CredorDeb = scanner.nextLine();
-
-        System.out.println("Enter the deposit amount: ");
-        double amount = Double.parseDouble(scanner.nextLine());
 
         System.out.println("Enter date (YYYY-MM-DD):");
         String date = scanner.nextLine();
 
+        //Time
+
+        System.out.println("What is your Name: ");
+        String name = scanner.nextLine();
+        //
+        System.out.println("Card Type Debit or Credit: ");
+        String CredorDeb = scanner.nextLine();
+
+        //Deposit
+        System.out.println("Enter the deposit amount: ");
+        double amount = Double.parseDouble(scanner.nextLine());
+
         //Ledger
         System.out.println("Ledger Opening Now............");
+        String ledger = scanner.nextLine();
 
         //Calls Reader and Writer Class
         Reader_and_Writer.saveDeposit(name, CredorDeb, amount, date);
