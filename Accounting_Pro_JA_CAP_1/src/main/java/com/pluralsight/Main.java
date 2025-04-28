e88415126ebad4fa4ebad17d0e2dce292e93d66a
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-04-28 08:40:45 -0400

    added a read from file

--- Accounting_Pro_JA_CAP_1/src/main/java/com/pluralsight/Main.java
+++ Accounting_Pro_JA_CAP_1/src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -8,12 +10,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
-        //Home Screen
-
         //Deposit
         System.out.println("What is your Name: ");
         String name = scanner.nextLine();
+
+        System.out.println("Card Type Debit or Credit: ");
+        String CredorDeb = scanner.nextLine();
 
         System.out.println("Enter the deposit amount: ");
         double amount = Double.parseDouble(scanner.nextLine());
@@ -22,13 +24,25 @@ public class Main {
         String date = scanner.nextLine();
 
         // Format: name,amount,date
-        String depositRecord = name + "," + amount + "," + date;
+        String depositRecord = name + "|" + CredorDeb + "|" + amount + "|" + date;
 
+        //Write to file
         try (FileWriter writer = new FileWriter("transactions.csv", true)) {
             writer.write(depositRecord + "\n");
             System.out.println("Deposit saved successfully.");
         } catch (IOException e) {
             System.out.println("Error writing to file: " + e.getMessage());
+        }
+
+        // Read from the file
+        System.out.println("Current contents of transactions.csv:");
+        try (BufferedReader reader = new BufferedReader(new FileReader("transactions.csv"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading from file: " + e.getMessage());
         }
     }
 }
