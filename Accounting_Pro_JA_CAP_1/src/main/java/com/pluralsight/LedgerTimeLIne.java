2a327d9a832c1b17f250e1bf91c7b2c2e7a82cd9
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-04-29 13:51:29 -0400

    created the buttons for ledger

--- Accounting_Pro_JA_CAP_1/src/main/java/com/pluralsight/LedgerTimeLIne.java
+++ Accounting_Pro_JA_CAP_1/src/main/java/com/pluralsight/LedgerTimeLIne.java
@@ -13,4 +13,33 @@ public class LedgerTimeLIne {
 //        loadTransactionsFromCSV("transactions.csv");
 //    }
 
+//    //HomeScreen
+//    public void showHome() {
+//        while (true) {
+//            System.out.println("\n=== Home Menu ===");
+//            System.out.println("D) Deposits");
+//            System.out.println("P) Payments");
+//            System.out.println("R) Reports");
+//            System.out.println("H) Home (Reload)");
+//            System.out.println("E) Exit");
+//            System.out.print("Enter your choice: ");
+//            String choice = scanner.nextLine().toUpperCase();
+//
+//            if (choice.equals("D")) {
+//                showDeposits();
+//            } else if (choice.equals("P")) {
+//                showPayments();
+//            } else if (choice.equals("R")) {
+//                showReports();
+//            } else if (choice.equals("H")) {
+//                System.out.println("Already at Home.");
+//            } else if (choice.equals("E")) {
+//                ExitHandler exitHandler = new ExitHandler(scanner);
+//                exitHandler.waitForExit();
+//            } else {
+//                System.out.println("Invalid input.");
+//            }
+//        }
+//    }
+
 }
