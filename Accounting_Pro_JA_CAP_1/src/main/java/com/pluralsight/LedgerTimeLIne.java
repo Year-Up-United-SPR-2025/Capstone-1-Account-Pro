a0ee765f36ba168392fd16ffa8e186536c4b227c
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-04-29 13:50:01 -0400

    created a class for ledger commits because I forgot to
    first I added the array list, scanner objects and had it be read from loadTransactionsFromCSV("transactions.csv");

---
+++ Accounting_Pro_JA_CAP_1/src/main/java/com/pluralsight/LedgerTimeLIne.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+//This is added because I forgot to add the ledger commits to GitHub
+public class LedgerTimeLIne {
+//    private List<Transaction> transactions = new ArrayList<>();
+//    private Scanner scanner = new Scanner(System.in);
+//
+//    public Ledger() {
+//        loadTransactionsFromCSV("transactions.csv");
+//    }
+
+}
