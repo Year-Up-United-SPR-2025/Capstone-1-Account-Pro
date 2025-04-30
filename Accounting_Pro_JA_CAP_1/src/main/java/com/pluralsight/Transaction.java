644d99760a0a11c850d0655d04d186fb93a86370
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-04-30 08:47:01 -0400

    added what was needed in Transaction class

--- Accounting_Pro_JA_CAP_1/src/main/java/com/pluralsight/Transaction.java
+++ Accounting_Pro_JA_CAP_1/src/main/java/com/pluralsight/Transaction.java
@@ -1,9 +1,17 @@
 package com.pluralsight;
 
 public class Transaction {
-    public int amount;
+    public String date;
     public String vendor;
+    public double amount;
 
     public Transaction(String date, String vendor, double amount) {
+        this.date = date;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+    
+    public String toString() {
+        return date + " | " + vendor + " | " + amount;
     }
 }
